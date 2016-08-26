78b20db753315f284bd745bd1439b9bc315fdc4e
SamC <sc14809@my.bristol.ac.uk>
2016-08-26 11:38:22 +0000

    silly big fixes

--- usbip-server.sh
+++ usbip-server.sh
@@ -8,10 +8,14 @@ if ! [ -z "$1" ]; then
 		usbip list -l
 	elif [ "$1" == "-n" ] || [ "$1" == "--netstat" ]; then
 		netstat -alpt
-	elif [ "$1" == "-b" ] || [ "$1" == "--bind"    ]; then 
-		sudo usbipd -D &
-		sudo usbip bind -b $1
-	elif ! [ -z "$2" ] && ([ "$1" == "-u" ] || [ "$1" == "--unbind"  ]); then
-		sudo usbip unbind -b $2
+	fi
+	
+	if ! [ -z "$2" ]; then
+		if   [ "$1" == "-b" ] || [ "$1" == "--bind"    ]; then 
+			sudo usbipd -D &
+			sudo usbip bind -b $2
+		elif [ "$1" == "-u" ] || [ "$1" == "--unbind"  ]; then
+			sudo usbip unbind -b $2
+		fi
 	fi
 fi
